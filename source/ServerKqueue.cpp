f7e4cc15bb2532f24b34302b62207fcbe0f4c569
Sangje An <sanan@c4r6s5.42seoul.kr>
2023-11-15 15:30:50 +0900

    DOCS : 서버 초기화 로직 주석 추가

--- source/ServerKqueue.cpp
+++ source/ServerKqueue.cpp
@@ -55,6 +55,9 @@ Server::Server(std::string port, std::string password) : opName(""), opPassword(
 		throw std::runtime_error("kqueue error!");
 }
 
+/**
+ * 클라이언트 맵을 지우고, 채팅 채널을 지운 후, kq를 닫는다.
+ */
 Server::~Server() {
 	for (cltmap::iterator it = clientList.begin(); it != clientList.end(); it++)
 		delete it->second;
@@ -63,33 +66,54 @@ Server::~Server() {
 	close(kq);
 }
 
+// 서버 초기화
 void Server::init() {
-	int yes = 1;
+	int yes = 1; // ?
 
-	this->servSock = socket(PF_INET, SOCK_STREAM, 0);
-
-	if (servSock == -1)
+	// 서버의 소켓을 연다. PF_INET는 IPv4,
+	// SOCK_STREAM은 TCP 프로토콜을 사용하는 연결 지향형 소켓
+	// socket() 함수는 PF_INET에 맞는 기본 프로토콜
+	if ((this->servSock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
 		throw std::runtime_error("Error : server socket is wrong");
 
+	// 서버의 주소 값을 초기화, socket_internet의 family, address, port를 지정
 	memset(&this->servAddr, 0, sizeof(this->servAddr));
 	this->servAddr.sin_family = AF_INET;
 	this->servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	this->servAddr.sin_port = htons(this->port);
 
+
+	// event를 kqueue에 추가한다.
+	// connectingFds는 현재 서버와 연결된 fd들에 대한 vector다.
+	// EVFILT_READ는 파일 디스크립터에서 읽기 가능한 데이터가 있는지를 검사하는 필터
+	// 이벤트를 추가, 활성화 한다.
 	pushEvents(this->connectingFds, this->servSock, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
 
-	// signal(SIGINT, SIG_IGN);
-
+	// 서버의 소켓 옵션을 설정한다. default 세팅이라고 생각하면 된다.
+	// SOL_SOCKET: 옵션의 레벨(level)을 지정합니다. SOL_SOCKET은 일반적인 소켓 옵션을 설정하는 데 사용
+	// SO_REUSEADDR: 설정하려는 옵션의 이름입니다. SO_REUSEADDR은 주로 TCP 소켓에서 사용되며, 이 옵션을 설정함으로써 이전에 사용된 주소와 포트를 즉시 재사용
+	// &yes: 옵션의 값을 설정하는 매개변수입니다. 여기서 yes는 int형 변수로, SO_REUSEADDR 옵션을 사용할 것이므로 일반적으로 1로 설정됩니다. 이는 해당 소켓이 이전에 사용된 주소를 재사용할 수 있도록 허용
+	// 옵션 값의 크기.
 	setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
+	// bind() 함수는 소켓에 주소를 할당하는 함수
+	// 서버 소켓에 주소를 할당한다.
 	if (bind(servSock, (struct sockaddr*)&servAddr, sizeof(servAddr)) == -1)
 		throw std::runtime_error("Error : bind");
+
+	// listen() 함수는 소켓을 연결 대기 상태로 만드는 함수
+	// 서버 소켓을 연결 대기 상태로 만든다.
 	if (listen(servSock, CONNECT) == -1)
 		throw std::runtime_error("Error : listen");
 
+	// fcntl() 함수는 파일 디스크립터의 플래그를 변경하는 함수
+	// 서브젝트 자체에서 해당 소켓을 논블로킹으로 설정하도록 얘기했음(MacOS의 경우).
 	fcntl(servSock, F_SETFL, O_NONBLOCK);
 
+	// 서버의 가동 상태를 의미하는 플래그
 	this->running = true;
+
+	// 서버 시작 시간 설정
 	this->startTime = getCurTime();
 }
 
